c95743e5464200f19d5131beb6674f4efdcb3b6a
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2020-03-05 16:53:33 +1100

    - final cleanup

--- assets/js/stylish-portfolio.js
+++ assets/js/stylish-portfolio.js
@@ -25,30 +25,21 @@ AOS.init();
     }
   });
 
+  // Adds box-shadow to navbar on scroll
+  $(document).scroll(function() {
+    if ($(document).scrollTop() >= 50) {
+      $("#navbar").addClass("box-shadow");
+    } else {
+      $("#navbar").removeClass("box-shadow");
+    }
+  });
+
   // Closes responsive menu when a scroll trigger link is clicked
   $("#navbarSupportedContent .js-scroll-trigger").click(function() {
     $("#navbarSupportedContent").removeClass("show");
-    $("#navbarSupportedContent").fadeOut();
+  });
+
+  $(".nav-link").click(function() {
+    $(".nav-link").removeClass("active");
   });
 })(jQuery); // End of use strict
-
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on("click", onMapClickHandler);
-  that.off("mouseleave", onMapMouseleaveHandler);
-  that.find("iframe").css("pointer-events", "none");
-};
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off("click", onMapClickHandler);
-  // Enable scrolling zoom
-  that.find("iframe").css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on("mouseleave", onMapMouseleaveHandler);
-};
-// Enable map zooming with mouse scroll when the user clicks the map
-$(".map").on("click", onMapClickHandler);
